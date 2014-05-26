7e440c3d01a9240eb6a005f3da2b069d7e337261
Hugo Gamboa <hgamboa@Hugos-MacBook-Pro.local>
2014-05-26 12:00:07 +0100

    peaks fix to work on older scipy versions

--- code/peaks.py
+++ code/peaks.py
@@ -5,8 +5,195 @@ peak finding module
 """
 
 from numpy import array, clip, argsort, sort
+import numpy as np
 from pylab import find
-from scipy.signal import argrelmax
+#from scipy.signal import argrelmax
+
+#### FROM SCIPY
+#### Given that the new version failed to compile in some linux versions
+
+
+
+def _boolrelextrema(data, comparator,
+                  axis=0, order=1, mode='clip'):
+    """
+    Calculate the relative extrema of `data`.
+
+    Relative extrema are calculated by finding locations where
+    ``comparator(data[n], data[n+1:n+order+1])`` is True.
+
+    Parameters
+    ----------
+    data : ndarray
+        Array in which to find the relative extrema.
+    comparator : callable
+        Function to use to compare two data points.
+        Should take 2 numbers as arguments.
+    axis : int, optional
+        Axis over which to select from `data`.  Default is 0.
+    order : int, optional
+        How many points on each side to use for the comparison
+        to consider ``comparator(n,n+x)`` to be True.
+    mode : str, optional
+        How the edges of the vector are treated.  'wrap' (wrap around) or
+        'clip' (treat overflow as the same as the last (or first) element).
+        Default 'clip'.  See numpy.take
+
+    Returns
+    -------
+    extrema : ndarray
+        Indices of the extrema, as boolean array of same shape as data.
+        True for an extrema, False else.
+
+    See also
+    --------
+    argrelmax, argrelmin
+
+    Examples
+    --------
+    >>> testdata = np.array([1,2,3,2,1])
+    >>> argrelextrema(testdata, np.greater, axis=0)
+    array([False, False,  True, False, False], dtype=bool)
+
+    """
+    if((int(order) != order) or (order < 1)):
+        raise ValueError('Order must be an int >= 1')
+
+    datalen = data.shape[axis]
+    locs = np.arange(0, datalen)
+
+    results = np.ones(data.shape, dtype=bool)
+    main = data.take(locs, axis=axis, mode=mode)
+    for shift in xrange(1, order + 1):
+        plus = data.take(locs + shift, axis=axis, mode=mode)
+        minus = data.take(locs - shift, axis=axis, mode=mode)
+        results &= comparator(main, plus)
+        results &= comparator(main, minus)
+        if(~results.any()):
+            return results
+    return results
+
+
+def argrelmin(data, axis=0, order=1, mode='clip'):
+    """
+    Calculate the relative minima of `data`.
+
+    .. versionadded:: 0.11.0
+
+    Parameters
+    ----------
+    data : ndarray
+        Array in which to find the relative minima.
+    axis : int, optional
+        Axis over which to select from `data`.  Default is 0.
+    order : int, optional
+        How many points on each side to use for the comparison
+        to consider ``comparator(n, n+x)`` to be True.
+    mode : str, optional
+        How the edges of the vector are treated.
+        Available options are 'wrap' (wrap around) or 'clip' (treat overflow
+        as the same as the last (or first) element).
+        Default 'clip'. See numpy.take
+
+    Returns
+    -------
+    extrema : ndarray
+        Indices of the minima, as an array of integers.
+
+    See also
+    --------
+    argrelextrema, argrelmax
+
+    Notes
+    -----
+    This function uses `argrelextrema` with np.less as comparator.
+
+    """
+    return argrelextrema(data, np.less, axis, order, mode)
+
+
+def argrelmax(data, axis=0, order=1, mode='clip'):
+    """
+    Calculate the relative maxima of `data`.
+
+    .. versionadded:: 0.11.0
+
+    Parameters
+    ----------
+    data : ndarray
+        Array in which to find the relative maxima.
+    axis : int, optional
+        Axis over which to select from `data`.  Default is 0.
+    order : int, optional
+        How many points on each side to use for the comparison
+        to consider ``comparator(n, n+x)`` to be True.
+    mode : str, optional
+        How the edges of the vector are treated.
+        Available options are 'wrap' (wrap around) or 'clip' (treat overflow
+        as the same as the last (or first) element).
+        Default 'clip'.  See `numpy.take`.
+
+    Returns
+    -------
+    extrema : ndarray
+        Indices of the maxima, as an array of integers.
+
+    See also
+    --------
+    argrelextrema, argrelmin
+
+    Notes
+    -----
+    This function uses `argrelextrema` with np.greater as comparator.
+
+    """
+    return argrelextrema(data, np.greater, axis, order, mode)
+
+
+def argrelextrema(data, comparator, axis=0, order=1, mode='clip'):
+    """
+    Calculate the relative extrema of `data`.
+
+    .. versionadded:: 0.11.0
+
+    Parameters
+    ----------
+    data : ndarray
+        Array in which to find the relative extrema.
+    comparator : callable
+        Function to use to compare two data points.
+        Should take 2 numbers as arguments.
+    axis : int, optional
+        Axis over which to select from `data`.  Default is 0.
+    order : int, optional
+        How many points on each side to use for the comparison
+        to consider ``comparator(n, n+x)`` to be True.
+    mode : str, optional
+        How the edges of the vector are treated.  'wrap' (wrap around) or
+        'clip' (treat overflow as the same as the last (or first) element).
+        Default is 'clip'.  See `numpy.take`.
+
+    Returns
+    -------
+    extrema : ndarray
+        Indices of the extrema, as an array of integers (same format as
+        np.argmin, np.argmax).
+
+    See also
+    --------
+    argrelmin, argrelmax
+
+    """
+    results = _boolrelextrema(data, comparator,
+                              axis, order, mode)
+    if ~results.any():
+        return (np.array([]),) * 2
+    else:
+        return np.where(results)
+
+
+#### FROM SCIPY 
+
 
 
 def peaks(signal, tol=None):
